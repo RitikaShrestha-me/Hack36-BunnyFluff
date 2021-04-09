3de8b603546b32fd4e747b93f8582fa2e7810c8e
RitikaShrestha-me <shrestharitika189@gmail.com>
2021-04-09 20:55:36 +0530

    -added components

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h2>BunnyFluff</h2>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/">How does it Work?</a>
+                <a href="/">Contact Us</a>
+                <button>Sign In</button>
+                <button>Get Contribution</button>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
