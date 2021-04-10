083423e28a1a88625bdbdbe5631a8de9be6ce1dc
Alisha Ahmed <alishaahmed2000@gmail.com>
2021-04-11 01:22:11 +0530

    login sidebox fix and About Us

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
 import Hero from './Hero'
 import About from './About'
 import Contributions from './Contributions'
+import Intro from './Intro';
 
 const Home = () => {
     return (
         <div>
             <Hero />
             <About />
+            <Intro />
             <Contributions />
         </div>
     );
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,12 +3,23 @@ import "../App.scss"
 
 const Intro = () => {
     return ( 
-        <div>
+        <div className="Intro">
             <div className="Aboutcontent">
+                <div className="about-intro">
+                    <h1>About Us</h1>
                 <p>
-                    Pandemic, A thing never imagined which had a severe effect on all the aspects of the world. Taking from Education, Work to the basic necessities of a small one-time meal which couldn’t be afforded by people with no money. All are aware of the down-fall in the percentage of jobs, income sources, and medium for people to strive for living.
-	                Where there have been people of so much scarcity of basic needs there are still many among us who can still afford these easily. And a Small amount one-time help, Contribution, Information Shared, Funds raised, Accessories donated shall help a needy for days. 
+                    Pandemic, A thing never imagined which had a severe effect on all the aspects of the world. Taking from Education, Work to the basic necessities of a small 
+                    one-time meal which couldn’t be afforded by people with no money. All are aware of the down-fall in the percentage of jobs, income sources, and medium for people to strive for living.
+	                Where there have been people of so much scarcity of basic needs there are still many among us who can still afford these easily. And a Small amount one-time
+                    help, Contribution, Information Shared, Funds raised, Accessories donated shall help a needy for days. 
                 </p>
+                <p>
+                    We at <b>Hope</b> have started an intiative where the needy ones can be supported with their requirements and the donor, fundraisers and Contributors <i>indeed the Hero's</i> 
+                    can find a platform during this pandemic to reach each other. And Support, Unite and Stand with all together.
+                </p>
+                <p></p>
+                <p><i>"Donate and Contribute and be the reason for someone's smile. Give what you can, Help whom you want..."</i></p>
+                </div>
             </div>
         </div>
     );
